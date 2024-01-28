cf224b7bb976257444d74237acdbab6c5ff202ad
jitheshm <jitheshmjithooz@gmail.com>
2024-01-28 19:30:13 +0530

    react router installed and pages routed in client side

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,14 +1,36 @@
 import React from 'react'
 import './App.css'
 import './assets/bootstrap.min.css'
-
+import {
+    createBrowserRouter,
+    RouterProvider,
+    useNavigate,
+  } from "react-router-dom";
+import Login from './pages/User/Login';
+import Signup from './pages/User/Signup';
+import ProductDetails from './pages/User/ProductDetails';
 
 
 
 function App() {
+    const router=createBrowserRouter([
+        {
+            path:"/login",
+            element:<Login/>
+        },
+        {
+            path:"/signup",
+            element:<Signup/>
+        },
+        {
+            path:"/product/:productId/:varientId",
+            element:<ProductDetails/>
+        }
+        
+    ])
     return (
         <>
-           
+            <RouterProvider router={router} />
         </>
     )
 
--- frontend/src/pages/User/Signup.jsx
+++ frontend/src/pages/User/Signup.jsx
@@ -1,11 +1,16 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Header from '../../components/User/Header/Header'
 import SignupComponent from '../../components/User/Signup/Signup'
+import OtpVerify from '../../components/User/Otp/OtpVerify'
 function Signup() {
+    const [verifyOtp, setVerifyOtp] = useState(false)
     return (
         <>
             <Header />
-            <SignupComponent />
+            {
+                verifyOtp ? <OtpVerify /> : <SignupComponent />
+            }
+           
         </>
     )
 }
