530c383c79a8b0a79b70ad4dbc6daa4e27da3f2c
jitheshm <jitheshmjithooz@gmail.com>
2024-01-28 14:05:54 +0530

    vite  app installed and header component added

---
+++ frontend/src/App.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import './App.css'
+import './assets/bootstrap.min.css'
+import Header from './components/Header/Header'
+
+
+
+function App() {
+    return (
+        <>
+            <Header />
+        </>
+    )
+
+}
+
+export default App
---
+++ frontend/src/components/Header/Header.jsx
@@ -0,0 +1,93 @@
+import React from 'react'
+import logo from '../../assets/logo.png'
+function Header() {
+    return (
+        <> 
+            <header>
+                {/* TOP HEADER */}
+                <div id="top-header">
+                    <div className="container">
+                        <ul className="header-links pull-left">
+                            <li><a href="#"><i className="fa fa-phone" /> +021-95-51-84</a></li>
+                            <li><a href="#"><i className="fa fa-envelope-o" /> email@email.com</a></li>
+                            <li><a href="#"><i className="fa fa-map-marker" /> 1734 Stonecoal Road</a></li>
+                        </ul>
+                        <ul className="header-links pull-right">
+                            <li><a href="#"><i className="fa fa-dollar" /> USD</a></li>
+                            <li><a href="#"><i className="fa fa-user-o" /> My Account</a></li>
+                        </ul>
+                    </div>
+                </div>
+                {/* /TOP HEADER */}
+                {/* MAIN HEADER */}
+                <div id="header">
+                    {/* container */}
+                    <div className="container-fluid">
+                        {/* row */}
+                        <div className="row">
+                            {/* LOGO */}
+                            <div className="col-md-3">
+                                <div className="header-logo">
+                                    <a href="#" className="logo">
+                                        <img src={logo} alt />
+                                    </a>
+                                </div>
+                            </div>
+                            {/* /LOGO */}
+                            {/* SEARCH BAR */}
+                            <div className="col-md-6">
+                                <div className="header-search">
+                                    <form>
+                                        <select className="input-select">
+                                            <option value={0}>All Categories</option>
+                                            <option value={1}>Category 01</option>
+                                            <option value={1}>Category 02</option>
+                                        </select>
+                                        <input className="input" placeholder="Search here" />
+                                        <button className="search-btn">Search</button>
+                                    </form>
+                                </div>
+                            </div>
+                            {/* /SEARCH BAR */}
+                            {/* ACCOUNT */}
+                            <div className="col-md-3  ">
+                                <div className="header-ctn d-flex">
+                                    {/* Wishlist */}
+                                    <div>
+                                        <a href="#">
+                                            <i className="fa-regular fa-heart" style={{ color: '#ffffff' }} />
+
+                                            <span>Your Wishlist</span>
+                                            <div className="qty">2</div>
+                                        </a>
+
+                                    </div>
+                                    {/* /Wishlist */}
+                                    {/* Cart */}
+                                    <div>
+                                        <a href="#">
+                                        <i className="fa-regular fa-heart" style={{ color: '#ffffff' }} />
+                                            <span>Your Wishlist</span>
+                                            <div className="qty">2</div>
+                                        </a>
+                                    </div>
+                                    {/* /Cart */}
+
+
+                                </div>
+                            </div>
+                            {/* /ACCOUNT */}
+                        </div>
+                        {/* row */}
+                    </div>
+                    {/* container */}
+                </div>
+                {/* /MAIN HEADER */}
+            </header>
+            {/* /HEADER */}
+
+        </>
+    )
+}
+
+export default Header
