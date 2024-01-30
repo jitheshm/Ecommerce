2cec18daf05b2149f0158ae10a42748ceaa81e2b
jitheshm <jitheshmjithooz@gmail.com>
2024-01-30 20:44:54 +0530

    product card split to different component

--- frontend/user/src/components/ProductCard/ProductCard.jsx
+++ frontend/user/src/components/ProductCard/ProductCard.jsx
@@ -4,8 +4,17 @@ import { Link } from 'react-router-dom'
 function ProductCard({ product }) {
     return (
         <>
-        
-            
+            <div className="card col-5 col-md-2 mt-5 mt-md-0 mx-2" >
+                <div className='p-3'>
+                    <img className="card-img-top" src="https://rukminim2.flixcart.com/image/416/416/xif0q/mobile/b/i/x/-original-imagt4qptrkzwmxa.jpeg?q=70&crop=false" alt="Card image cap" />
+                </div>
+                <div className="card-body">
+                    <h5 className="card-title "><b>Realme 9 pro plus</b></h5>
+
+                    <p className="card-text mt-2"><b>₹ 19,999</b> <b style={{ color: "green" }}>20% OFF</b></p>
+                </div>
+            </div>
+
         </>
     )
 }
--- frontend/user/src/components/ProductSlide/ProductSlide.jsx
+++ frontend/user/src/components/ProductSlide/ProductSlide.jsx
@@ -23,66 +23,11 @@ function ProductSlide({ title }) {
                 </div>
                 <div className=' m-md-5 d-flex gap-md-5 ms-4 slidebar '>
                     
-                    <div className="card col-5 col-md-2 mt-5 mt-md-0 mx-2" >
-                        <div className='p-3'>
-                            <img className="card-img-top" src="https://rukminim2.flixcart.com/image/416/416/xif0q/mobile/b/i/x/-original-imagt4qptrkzwmxa.jpeg?q=70&crop=false" alt="Card image cap" />
-                        </div>
-                        <div className="card-body">
-                            <h5 className="card-title "><b>Realme 9 pro plus</b></h5>
-                            
-                            <p className="card-text mt-2"><b>₹ 19,999</b> <b style={{color:"green"}}>20% OFF</b></p>
-                        </div>
-                    </div> 
-                    <div className="card col-5 col-md-2 mt-5 mt-md-0 mx-2" >
-                        <div className='p-3'>
-                            <img className="card-img-top" src="https://rukminim2.flixcart.com/image/416/416/xif0q/mobile/b/i/x/-original-imagt4qptrkzwmxa.jpeg?q=70&crop=false" alt="Card image cap" />
-                        </div>
-                        <div className="card-body">
-                            <h5 className="card-title "><b>Realme 9 pro plus</b></h5>
-                            
-                            <p className="card-text mt-2"><b>₹ 19,999</b> <b style={{color:"green"}}>20% OFF</b></p>
-                        </div>
-                    </div> 
-                    <div className="card col-5 col-md-2 mt-5 mt-md-0 mx-2" >
-                        <div className='p-3'>
-                            <img className="card-img-top" src="https://rukminim2.flixcart.com/image/416/416/xif0q/mobile/b/i/x/-original-imagt4qptrkzwmxa.jpeg?q=70&crop=false" alt="Card image cap" />
-                        </div>
-                        <div className="card-body">
-                            <h5 className="card-title "><b>Realme 9 pro plus</b></h5>
-                            
-                            <p className="card-text mt-2"><b>₹ 19,999</b> <b style={{color:"green"}}>20% OFF</b></p>
-                        </div>
-                    </div> 
-                    <div className="card col-5 col-md-2 mt-5 mt-md-0 mx-2" >
-                        <div className='p-3'>
-                            <img className="card-img-top" src="https://rukminim2.flixcart.com/image/416/416/xif0q/mobile/b/i/x/-original-imagt4qptrkzwmxa.jpeg?q=70&crop=false" alt="Card image cap" />
-                        </div>
-                        <div className="card-body">
-                            <h5 className="card-title "><b>Realme 9 pro plus</b></h5>
-                            
-                            <p className="card-text mt-2"><b>₹ 19,999</b> <b style={{color:"green"}}>20% OFF</b></p>
-                        </div>
-                    </div> 
-                    <div className="card col-5 col-md-2 mt-5 mt-md-0 mx-2" >
-                        <div className='p-3'>
-                            <img className="card-img-top" src="https://rukminim2.flixcart.com/image/416/416/xif0q/mobile/b/i/x/-original-imagt4qptrkzwmxa.jpeg?q=70&crop=false" alt="Card image cap" />
-                        </div>
-                        <div className="card-body">
-                            <h5 className="card-title "><b>Realme 9 pro plus</b></h5>
-                            
-                            <p className="card-text mt-2"><b>₹ 19,999</b> <b style={{color:"green"}}>20% OFF</b></p>
-                        </div>
-                    </div> 
-                    <div className="card col-5 col-md-2 mt-5 mt-md-0 mx-2" >
-                        <div className='p-3'>
-                            <img className="card-img-top" src="https://rukminim2.flixcart.com/image/416/416/xif0q/mobile/b/i/x/-original-imagt4qptrkzwmxa.jpeg?q=70&crop=false" alt="Card image cap" />
-                        </div>
-                        <div className="card-body">
-                            <h5 className="card-title "><b>Realme 9 pro plus</b></h5>
-                            
-                            <p className="card-text mt-2"><b>₹ 19,999</b> <b style={{color:"green"}}>20% OFF</b></p>
-                        </div>
-                    </div> 
+                    <ProductCard/> 
+                    <ProductCard/> 
+                    <ProductCard/> 
+                    <ProductCard/> 
+                    <ProductCard/> 
                      
                     
                     
