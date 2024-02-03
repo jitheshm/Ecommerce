6391f84f1eaccd467411a7625ff1769a62a2d82d
jitheshm <jitheshmjithooz@gmail.com>
2024-02-03 19:42:29 +0530

    category list page updated

--- frontend/admin/src/components/CategoryList/CategoryList.jsx
+++ frontend/admin/src/components/CategoryList/CategoryList.jsx
@@ -4,6 +4,7 @@ import Cookies from 'js-cookie';
 import { Link } from 'react-router-dom';
 function CategoryList() {
     const [category, setCategory] = useState([])
+    const [search, setSearch] = useState('')
     useEffect(() => {
         instance.get('/admin/getcategories', {
             headers: {
@@ -24,51 +25,76 @@ function CategoryList() {
                 }
             }).then((res) => {
                 console.log(res);
-                setCategory(category.filter((obj)=>obj._id!==id))
+                setCategory(category.filter((obj) => obj._id !== id))
             })
         }
 
     }
 
+    const handleSearch = (e) => {
+        setSearch(e.target.value)
+    }
+
     return (
-        <div className='mt-5 pt-2'>
-            <Link to={'/addcategory'} className='btn btn-primary mt-5 mx-5'>Add Category</Link>
-            <table className="table table-dark mt-5 pt-5">
-                <thead className=''>
-                    <tr>
-                        <th scope="col">#</th>
-                        <th scope="col">Name</th>
-                        
-                        <th scope="col">Actions</th>
+        <>
+            <div className='pt-5'>
+                <div className="col-lg-10 mt-5 m-auto grid-margin stretch-card"> 
+                    <div className="card">
+                        <div className="card-body">
+                            <div className='row align-items-center mb-4'>
+                                <h4 className="card-title col-4 mt-4">Categories</h4>
+                                <form className="nav-link  d-none d-lg-flex search col-6">
+                                    <input type="text" className="form-control" placeholder="Search categories" style={{ color: "white" }} onChange={handleSearch} />
+                                </form>
+                            </div>
 
-                    </tr>
-                </thead>
-                <tbody>
-                    {
-                        category.map((obj) => {
-                            return (
-                                // eslint-disable-next-line react/jsx-key
-                                <tr>
-                                    <th scope="row">1</th>
-                                    <td>{obj.category}</td>
-                                   
-                                    <td>
-                                        <Link to={`/editcategory/${obj._id}`}>edit</Link>
-                                        <button onClick={() => {
-                                            handleDelete(obj._id)
-                                        }}>delete</button>
 
-                                       
+                            <div className="table-responsive col-12">
+                                <table className="table text-center" style={{ tableLayout: 'fixed' }}>
+                                    <thead>
+                                        <tr>
+                                            <th style={{ width: "50px" }}>Index</th>
+                                            <th>CID</th>
+                                            <th>Name</th>
+                                            <th style={{ textAlign: "center", width: "200px" }}>Actions</th>
+                                        </tr>
+                                    </thead>
+                                    <tbody >
+                                        {
+                                            category.map((catObj, index) => {
+                                                if (catObj.category.startsWith(search) || search === "") {
+                                                    return (
+                                                        // eslint-disable-next-line react/jsx-key
+                                                        <tr >
+                                                            <td style={{ color: "#bcc0d7", width: "50px" }}>{index + 1}</td>
+                                                            <td style={{ color: "#bcc0d7" }}>{catObj._id}</td>
+                                                            <td style={{ color: "#bcc0d7" }}>{catObj.category}</td>
+                                                            <td className='d-flex gap-3 justify-content-center' style={{ width: "200px" }}>
 
-                                    </td>
-                                </tr>
-                            )
-                        })
-                    }
+                                                                <Link to={`/editcategory/${catObj._id}`} className='btn btn-outline-warning'>Edit</Link>
 
-                </tbody>
-            </table>
-        </div>
+
+                                                                <button className='btn btn-outline-danger' onClick={() => handleDelete(catObj._id)}>Delete</button>
+
+
+                                                            </td>
+                                                        </tr>
+                                                    )
+                                                } else {
+                                                    return null
+                                                }
+                                            })
+                                        }
+
+                                    </tbody>
+                                </table>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+        </>
 
     )
 }
