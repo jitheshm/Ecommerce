d6d56e65a3d1fe3f77a4b45989cd5b2acf282fe0
jitheshm <jitheshmjithooz@gmail.com>
2024-02-04 00:10:17 +0530

    edit product component updated

--- frontend/admin/src/pages/EditProduct.jsx
+++ frontend/admin/src/pages/EditProduct.jsx
@@ -1,45 +1,13 @@
 import React from 'react'
 import ProductForm from '../components/ProductForm/ProductForm'
-import { useEffect } from 'react'
-import { useState } from 'react'
-import instance from '../axios'
-import { useParams } from 'react-router-dom'
-import Cookies from 'js-cookie';
+import { useParams } from 'react-router-dom'  
+
 function EditProduct() {
-    const [productName, setProductName] = useState("")
-    const [brand, setBrand] = useState("")
-    const [categoryId, setCategoryId] = useState("")
-    const [aboutProduct, setAboutProduct] = useState("")
-    const [isListed, setIsListed] = useState(true)
-    const [waranty, setWaranty] = useState(0)
-    const [availableCategory, setAvailableCategory] = useState([])
+    
     const {id} = useParams()
 
-   
-
-
-
-    const props = {
-        productName,
-        setProductName,
-        brand,
-        setBrand,
-        categoryId,
-        setCategoryId,
-        aboutProduct,
-        setAboutProduct,
-        isListed,
-        setIsListed,
-        waranty,
-        setWaranty,
-        availableCategory,
-        setAvailableCategory,
-        api: '/admin/updateproduct',
-        method:'patch',
-        id
-    }
     return (
-        <ProductForm {...props} title="Edit product details" btnName="Update Product" />
+        <ProductForm  title="Edit product details" api="/admin/updateproduct" method="patch" btnName="Update Product" id={id} />
     )
 }
 
