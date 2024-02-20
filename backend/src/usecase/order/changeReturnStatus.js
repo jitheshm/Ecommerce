c7cd697ebbfb6c7c46a7f36fdc74491963415b15
jitheshm <jitheshmjithooz@gmail.com>
2024-02-20 09:18:06 +0530

    refund function updated

--- backend/src/usecase/order/changeReturnStatus.js
+++ backend/src/usecase/order/changeReturnStatus.js
@@ -1,8 +1,4 @@
-module.exports = async (orderRepository, walletRepository, orderId, productId, status) => {
+module.exports = async (orderRepository,orderId, productId, status) => {
     await orderRepository.changeReturnStatus(orderId, productId, status)
-    const order = await orderRepository.getOrderSpecificProduct(orderId, productId)
-    if (status === 'Refund') {
-        console.log("haii");
-        return await walletRepository.addFund(order[0].userId, order[0].orderedItems.price, 'Refund')
-    }
+    
 }
