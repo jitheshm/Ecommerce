21c344b8e8d347fe46db55f4601a3d2894050b73
jitheshm <jitheshmjithooz@gmail.com>
2024-02-23 22:48:42 +0530

    image cropping feature added

---
+++ frontend/admin/src/components/CropImage/CropImage.jsx
@@ -0,0 +1,209 @@
+
+
+import React, { useState } from 'react'
+import Cropper from 'react-easy-crop'
+import './CropImage.css'
+import { BASEURL } from "../../constants/constant.json"
+import Resizer from "react-image-file-resizer";
+function CropImage({ src, setImage, setImgPrev, setCropWindow, id, setOldImage }) {
+
+
+    const [crop, setCrop] = useState({ x: 0, y: 0 })
+    const [rotation, setRotation] = useState(0)
+    const [zoom, setZoom] = useState(1)
+    const [croppedAreaPixels, setCroppedAreaPixels] = useState(null)
+    //const [croppedImage, setCroppedImage] = useState(null)
+
+
+
+    const createImage = (url) =>
+        new Promise((resolve, reject) => {
+            const image = new Image()
+            image.addEventListener('load', () => resolve(image))
+            image.addEventListener('error', (error) => reject(error))
+            image.setAttribute('crossOrigin', 'anonymous') // needed to avoid cross-origin issues on CodeSandbox
+            image.src = url
+        })
+    const getRadianAngle = (degreeValue) => {
+        return (degreeValue * Math.PI) / 180
+    }
+
+    /**
+     * Returns the new bounding area of a rotated rectangle.
+     */
+    const rotateSize = (width, height, rotation) => {
+        const rotRad = getRadianAngle(rotation)
+
+        return {
+            width:
+                Math.abs(Math.cos(rotRad) * width) + Math.abs(Math.sin(rotRad) * height),
+            height:
+                Math.abs(Math.sin(rotRad) * width) + Math.abs(Math.cos(rotRad) * height),
+        }
+    }
+
+    /**
+     * This function was adapted from the one in the ReadMe of https://github.com/DominicTobias/react-image-crop
+     */
+    /**
+     * This function was adapted from the one in the ReadMe of https://github.com/DominicTobias/react-image-crop
+     */
+    const getCroppedImg = async (
+        imageSrc,
+        pixelCrop,
+        rotation = 0,
+        flip = { horizontal: false, vertical: false }
+    ) => {
+        const image = await createImage(imageSrc)
+        const canvas = document.createElement('canvas')
+        const ctx = canvas.getContext('2d')
+
+        if (!ctx) {
+            return null
+        }
+
+        const rotRad = getRadianAngle(rotation)
+
+        // calculate bounding box of the rotated image
+        const { width: bBoxWidth, height: bBoxHeight } = rotateSize(
+            image.width,
+            image.height,
+            rotation
+        )
+
+        // set canvas size to match the bounding box
+        canvas.width = bBoxWidth
+        canvas.height = bBoxHeight
+        ctx.fillStyle = 'white';
+        ctx.fillRect(0, 0, canvas.width, canvas.height);
+        // translate canvas context to a central location to allow rotating and flipping around the center
+        ctx.translate(bBoxWidth / 2, bBoxHeight / 2)
+        ctx.rotate(rotRad)
+        ctx.scale(flip.horizontal ? -1 : 1, flip.vertical ? -1 : 1)
+        ctx.translate(-image.width / 2, -image.height / 2)
+
+        // draw rotated image
+        ctx.drawImage(image, 0, 0)
+
+        const croppedCanvas = document.createElement('canvas')
+
+        const croppedCtx = croppedCanvas.getContext('2d')
+
+        if (!croppedCtx) {
+            return null
+        }
+
+        // Set the size of the cropped canvas
+        croppedCanvas.width = pixelCrop.width
+        croppedCanvas.height = pixelCrop.height
+        console.log(canvas.width, canvas.height);
+
+        // Draw the cropped image onto the new canvas
+        croppedCtx.drawImage(
+            canvas,
+            pixelCrop.x,
+            pixelCrop.y,
+            pixelCrop.width,
+            pixelCrop.height,
+            0,
+            0,
+            pixelCrop.width,
+            pixelCrop.height
+        )
+
+        // As Base64 string
+        // return croppedCanvas.toDataURL('image/jpeg');
+
+        // As a blob
+
+        croppedCanvas.toBlob(async (file) => {
+
+            const url = URL.createObjectURL(file)
+            setImage(file)
+            setImgPrev((prev) => {
+                if (id && prev) {
+                    console.log("iam here");
+                    let newUrl = prev.replace(new RegExp('^' + `${BASEURL}/`), '');
+                    console.log(newUrl);
+                    setOldImage((prev) => {
+                        return [...prev, newUrl]
+                    })
+
+                }
+                return url
+            })
+            setCropWindow(false)
+            // setOldImage(url)
+        })
+
+    }
+
+
+
+
+
+    const onCropComplete = (croppedArea, croppedAreaPixels) => {
+        setCroppedAreaPixels(croppedAreaPixels)
+    }
+
+    const onDone = async () => {
+        try {
+            await getCroppedImg(
+                src,
+                croppedAreaPixels,
+                rotation
+            )
+
+        } catch (e) {
+            console.error(e)
+        }
+    }
+
+
+
+
+
+    return (
+        <>
+            <div className='cropParent col-10 '>
+
+
+                <div className='cropContainer'>
+                    <Cropper
+                        image={src}
+                        crop={crop}
+                        zoom={zoom}
+                        rotation={rotation}
+                        aspect={353 / 416}
+                        onCropChange={setCrop}
+                        onCropComplete={onCropComplete}
+                        onZoomChange={setZoom}
+                        onRotationChange={setRotation}
+                        objectFit='contain'
+                        restrictPosition={false}
+                        cropSize={{ width: 353, height: 416 }}
+                    />
+                </div>
+                <div className='controls'>
+                    <div >
+                        <div style={{ display: 'flex', gap: 5 }}>
+                            <label htmlFor="customRange1" className="form-label">Rotation</label>
+                            <input type="range" className="form-range" id="customRange1" min={0} max={360} step={1} onChange={(e) => {
+                                console.log(rotation);
+                                setRotation(e.target.value)
+                            }} value={rotation} />
+                        </div>
+
+                    </div>
+                    <button type='button' className='btn btn-primary' onClick={() => {
+                        onDone()
+                    }}>Done</button>
+                </div>
+            </div>
+
+
+        </>
+    )
+}
+
+export default CropImage
--- frontend/admin/src/components/VarientForm/VarientForm.jsx
+++ frontend/admin/src/components/VarientForm/VarientForm.jsx
@@ -4,7 +4,8 @@ import instance from '../../axios';
 import Cookies from 'js-cookie';
 import { useNavigate } from 'react-router-dom';
 import { BASEURL } from "../../constants/constant.json"
-import Resizer from "react-image-file-resizer";
+
+import CropImage from '../CropImage/CropImage';
 
 function VarientForm({ api, method, title, btnName, proId, id }) {
 
@@ -26,25 +27,12 @@ function VarientForm({ api, method, title, btnName, proId, id }) {
   const [colorError, setColorError] = useState(false)
   const [imgError, setImgError] = useState(false)
   const [oldImage, setOldImage] = useState([])
-
+  const [cropWindow, setCropWindow] = useState(false)
+  const [cropComponent, setCropComponent] = useState(null)
   const navigate = useNavigate()
 
 
-  const resizeFile = (file) =>
-    new Promise((resolve) => {
-      Resizer.imageFileResizer(
-        file,
-        353,
-        416,
-        "JPEG",
-        100,
-        0,
-        (uri) => {
-          resolve(uri);
-        },
-        'blob'
-      );
-    });
+  
 
   useEffect(() => {
     if (id) {
@@ -71,26 +59,30 @@ function VarientForm({ api, method, title, btnName, proId, id }) {
   const handleImageChange = async (setImage, setImgPre, e) => {
 
     setImgError(false)
-    const image = await resizeFile(e.target.files[0]);
-    setImage(image);
+    //const image = await resizeFile(e.target.files[0]);
+    const image = e.target.files[0]
+    //setImage(image);
     console.log("hallo");
     let file = image
     if (file) {
       const reader = new FileReader();
       reader.onloadend = () => {
-        setImgPre((prev) => {
-          if (id && prev) {
-            console.log("iam here");
-            let newUrl = prev.replace(new RegExp('^' + `${BASEURL}/`), '');
-            console.log(newUrl);
-            setOldImage((prev) => {
-              return [...prev, newUrl]
-            })
-            console.log(oldImage);
-          }
-          return reader.result
-        }
-        );
+
+        setCropComponent(<CropImage src={reader.result} setCropWindow={setCropWindow} setImage={setImage} setImgPrev={setImgPre} id={id} setOldImage={setOldImage} />)
+        setCropWindow(true)
+        // setImgPre((prev) => {
+        //   if (id && prev) {
+        //     console.log("iam here");
+        //     let newUrl = prev.replace(new RegExp('^' + `${BASEURL}/`), '');
+        //     console.log(newUrl);
+        //     setOldImage((prev) => {
+        //       return [...prev, newUrl]
+        //     })
+        //     console.log(oldImage);
+        //   }
+        //   return reader.result
+        // }
+        //);
       };
       reader.readAsDataURL(file);
     } else {
@@ -192,171 +184,177 @@ function VarientForm({ api, method, title, btnName, proId, id }) {
 
   return (
     <>
-      <div className='pt-5'>
-        <div className="col-11 m-auto mt-5 grid-margin">
-          <div className="card">
-            <div className="card-body mt-3">
-              <h4 className="card-title ">{title}</h4>
-              <form className="form-sample mt-5 ">
+      {
+        cropWindow ?
+          cropComponent
+          :
+          <div className='pt-5'>
+            <div className="col-11 m-auto mt-5 grid-margin">
+              <div className="card">
+                <div className="card-body mt-3">
+                  <h4 className="card-title ">{title}</h4>
+                  <form className="form-sample mt-5 ">
 
-                <div className="row mt-1">
-                  <div className="col-md-6">
-                    <div className="form-group row">
-                      <label className="col-sm-3 col-form-label">Color</label>
-                      <div className="col-sm-9">
-                        <input type="text" className="form-control text-white" value={color} onChange={(e) => {
-                          setColor(e.target.value)
-                          setColorError(false)
+                    <div className="row mt-1">
+                      <div className="col-md-6">
+                        <div className="form-group row">
+                          <label className="col-sm-3 col-form-label">Color</label>
+                          <div className="col-sm-9">
+                            <input type="text" className="form-control text-white" value={color} onChange={(e) => {
+                              setColor(e.target.value)
+                              setColorError(false)
 
-                        }} />
-                        <div style={{ height: "30px" }}>
-                          {colorError && <p className='text-danger'>Color is required</p>}
+                            }} />
+                            <div style={{ height: "30px" }}>
+                              {colorError && <p className='text-danger'>Color is required</p>}
+                            </div>
+                          </div>
+                        </div>
+                      </div>
+                      <div className="col-md-6">
+                        <div className="form-group row">
+                          <label className="col-sm-3 col-form-label">Stock</label>
+                          <div className="col-sm-9">
+                            <input type="number" className="form-control text-white" value={stock} onChange={(e) => {
+                              setStockError(false)
+                              setStock(e.target.value)
+
+                            }} />
+                            <div style={{ height: "30px" }}>
+                              {stockError && <p className='text-danger'>Stock must be a positive number or zero</p>}
+                            </div>
+                          </div>
                         </div>
                       </div>
                     </div>
-                  </div>
-                  <div className="col-md-6">
-                    <div className="form-group row">
-                      <label className="col-sm-3 col-form-label">Stock</label>
-                      <div className="col-sm-9">
-                        <input type="number" className="form-control text-white" value={stock} onChange={(e) => {
-                          setStockError(false)
-                          setStock(e.target.value)
 
-                        }} />
-                        <div style={{ height: "30px" }}>
-                          {stockError && <p className='text-danger'>Stock must be a positive number or zero</p>}
+                    <div className="row mt-1">
+
+                      <div className="col-md-6">
+                        <div className="form-group row">
+                          <label className="col-sm-3 col-form-label">Actual Price</label>
+                          <div className="col-sm-9">
+                            <input type="number" className="form-control text-white" value={actualPrice} min={0} onChange={(e) => {
+                              setActualPrice(e.target.value)
+                              setActualPriceError(false)
+                            }} />
+                            <div style={{ height: "30px" }}>
+                              {actualPriceError && <p className='text-danger'>Actual Price must be a positive number</p>}
+                            </div>
+                          </div>
+                        </div>
+                      </div>
+                      <div className="col-md-6">
+                        <div className="form-group row">
+                          <label className="col-sm-3 col-form-label">Sale Price</label>
+                          <div className="col-sm-9">
+                            <input type="number" className="form-control text-white" value={salePrice} min={0} onChange={(e) => {
+                              setSalePrice(e.target.value)
+                              setSalePriceError(false)
+                            }} />
+                            <div style={{ height: "30px" }}>
+                              {salePriceError && <p className='text-danger'>sale price is required</p>}
+                            </div>
+                          </div>
                         </div>
                       </div>
                     </div>
-                  </div>
+
+
+                    {/* image div starting */}
+
+
+                    <div className='row col-8 m-auto mt-2'>
+                      <div className='col-3 text-center'>
+                        <div className="mb-4 d-flex justify-content-center">
+                          <div>
+                            <label htmlFor='file-1' id='file1-label'>
+                              <img id="selectedImage1" src={imagePre1 ? imagePre1 : img} style={{ width: "100%", height: "150px" }} />
+                            </label>
+                            <div>
+                              <button type='button' className='btn btn-outline-secondary mt-3' onClick={() => {
+                                handleRemove(imagePre1, setImagePre1, image1, setImage1)
+                              }}>Remove</button>
+                            </div>
+                          </div>
+                        </div>
+                        <div className="d-flex justify-content-center">
+                          <input id='file-1' type="file" onChange={(e) => {
+                            handleImageChange(setImage1, setImagePre1, e)
+                          }} style={{ display: "none" }} />
+                        </div>
+                      </div>
+                      <div className='col-3'>
+                        <div className="mb-4 d-flex justify-content-center">
+                          <div>
+                            <label htmlFor='file-2' id='file2-label' >
+                              <img id="selectedImage" src={imagePre2 ? imagePre2 : img} style={{ width: "100%", height: "150px" }} />
+                            </label>
+                            <div className='text-center'>
+                              <button type='button' className='btn btn-outline-secondary mt-3' onClick={() => {
+                                handleRemove(imagePre2, setImagePre2, image2, setImage2)
+                              }}>Remove</button>
+                            </div>
+                          </div>
+                        </div>
+                        <input id='file-2' type="file" onChange={(e) => {
+                          handleImageChange(setImage2, setImagePre2, e)
+
+                        }} style={{ display: "none" }} />
+                      </div>
+                      <div className='col-3'>
+                        <div className="mb-4 d-flex justify-content-center">
+                          <div>
+                            <label htmlFor='file-3' id='file3-label' >
+                              <img id="selectedImage" src={imagePre3 ? imagePre3 : img} style={{ width: "100%", height: "150px" }} />
+                            </label>
+                            <div className='text-center'>
+                              <button type='button' className='btn btn-outline-secondary mt-3' onClick={() => {
+                                handleRemove(imagePre3, setImagePre3, image3, setImage3)
+                              }}>Remove</button>
+                            </div>
+                          </div>
+                        </div>
+                        <div className="d-flex justify-content-center">
+                          <input id='file-3' type="file" onChange={(e) => {
+                            handleImageChange(setImage3, setImagePre3, e)
+
+                          }} style={{ display: "none" }} />
+                        </div>
+                      </div>
+                      <div className='col-3'>
+                        <div className="mb-4 d-flex justify-content-center">
+                          <div>
+                            <label htmlFor='file-4' id='file4-label' >
+                              <img id="selectedImage" src={imagePre4 ? imagePre4 : img} alt="example placeholder" style={{ width: "100%", height: "150px" }} />
+                            </label>
+                            <div className='text-center'>
+                              <button type='button' className='btn btn-outline-secondary mt-3' onClick={() => {
+                                handleRemove(imagePre4, setImagePre4, image4, setImage4)
+                              }}>Remove</button>
+                            </div>
+                          </div>
+                        </div>
+                        <div className="d-flex justify-content-center">
+                          <input id='file-4' type="file" onChange={(e) => {
+                            handleImageChange(setImage4, setImagePre4, e)
+
+
+                          }} style={{ display: "none" }} />
+                        </div>
+                      </div>
+                    </div>
+                    {imgError && <p className='text-danger text-center'>Atleast  one image is required</p>}
+
+
+                    <button type='button' className='btn-inverse-success mt-4' onClick={handleSubmit}>{btnName}</button>
+                  </form>
                 </div>
-
-                <div className="row mt-1">
-
-                  <div className="col-md-6">
-                    <div className="form-group row">
-                      <label className="col-sm-3 col-form-label">Actual Price</label>
-                      <div className="col-sm-9">
-                        <input type="number" className="form-control text-white" value={actualPrice} min={0} onChange={(e) => {
-                          setActualPrice(e.target.value)
-                          setActualPriceError(false)
-                        }} />
-                        <div style={{ height: "30px" }}>
-                          {actualPriceError && <p className='text-danger'>Actual Price must be a positive number</p>}
-                        </div>
-                      </div>
-                    </div>
-                  </div>
-                  <div className="col-md-6">
-                    <div className="form-group row">
-                      <label className="col-sm-3 col-form-label">Sale Price</label>
-                      <div className="col-sm-9">
-                        <input type="number" className="form-control text-white" value={salePrice} min={0} onChange={(e) => {
-                          setSalePrice(e.target.value)
-                          setSalePriceError(false)
-                        }} />
-                        <div style={{ height: "30px" }}>
-                          {salePriceError && <p className='text-danger'>sale price is required</p>}
-                        </div>
-                      </div>
-                    </div>
-                  </div>
-                </div>
-
-
-                {/* image div starting */}
-
-
-                <div className='row col-8 m-auto mt-2'>
-                  <div className='col-3 text-center'>
-                    <div className="mb-4 d-flex justify-content-center">
-                      <div>
-                        <label htmlFor='file-1' id='file1-label'>
-                          <img id="selectedImage1" src={imagePre1 ? imagePre1 : img} style={{ width: "100%", height: "150px" }} />
-                        </label>
-                        <div>
-                          <button type='button' className='btn btn-outline-secondary mt-3' onClick={() => {
-                            handleRemove(imagePre1, setImagePre1, image1, setImage1)
-                          }}>Remove</button>
-                        </div>
-                      </div>
-                    </div>
-                    <div className="d-flex justify-content-center">
-                      <input id='file-1' type="file" onChange={(e) => {
-                        handleImageChange(setImage1, setImagePre1, e)
-                      }} style={{ display: "none" }} />
-                    </div>
-                  </div>
-                  <div className='col-3'>
-                    <div className="mb-4 d-flex justify-content-center">
-                      <div>
-                        <label htmlFor='file-2' id='file2-label' >
-                          <img id="selectedImage" src={imagePre2 ? imagePre2 : img} style={{ width: "100%", height: "150px" }} />
-                        </label>
-                        <div className='text-center'>
-                          <button type='button' className='btn btn-outline-secondary mt-3' onClick={() => {
-                            handleRemove(imagePre2, setImagePre2, image2, setImage2)
-                          }}>Remove</button>
-                        </div>
-                      </div>
-                    </div>
-                    <input id='file-2' type="file" onChange={(e) => {
-                      handleImageChange(setImage2, setImagePre2, e)
-
-                    }} style={{ display: "none" }} />
-                  </div>
-                  <div className='col-3'>
-                    <div className="mb-4 d-flex justify-content-center">
-                      <div>
-                        <label htmlFor='file-3' id='file3-label' >
-                          <img id="selectedImage" src={imagePre3 ? imagePre3 : img} style={{ width: "100%", height: "150px" }} />
-                        </label>
-                        <div className='text-center'>
-                          <button type='button' className='btn btn-outline-secondary mt-3' onClick={() => {
-                            handleRemove(imagePre3, setImagePre3, image3, setImage3)
-                          }}>Remove</button>
-                        </div>
-                      </div>
-                    </div>
-                    <div className="d-flex justify-content-center">
-                      <input id='file-3' type="file" onChange={(e) => {
-                        handleImageChange(setImage3, setImagePre3, e)
-
-                      }} style={{ display: "none" }} />
-                    </div>
-                  </div>
-                  <div className='col-3'>
-                    <div className="mb-4 d-flex justify-content-center">
-                      <div>
-                        <label htmlFor='file-4' id='file4-label' >
-                          <img id="selectedImage" src={imagePre4 ? imagePre4 : img} alt="example placeholder" style={{ width: "100%", height: "150px" }} />
-                        </label>
-                        <div className='text-center'>
-                          <button type='button' className='btn btn-outline-secondary mt-3' onClick={() => {
-                            handleRemove(imagePre4, setImagePre4, image4, setImage4)
-                          }}>Remove</button>
-                        </div>
-                      </div>
-                    </div>
-                    <div className="d-flex justify-content-center">
-                      <input id='file-4' type="file" onChange={(e) => {
-                        handleImageChange(setImage4, setImagePre4, e)
-
-
-                      }} style={{ display: "none" }} />
-                    </div>
-                  </div>
-                </div>
-                {imgError && <p className='text-danger text-center'>Atleast  one image is required</p>}
-
-
-                <button type='button' className='btn-inverse-success mt-4' onClick={handleSubmit}>{btnName}</button>
-              </form>
+              </div>
             </div>
           </div>
-        </div>
-      </div>
+      }
+
     </>
   )
 }
