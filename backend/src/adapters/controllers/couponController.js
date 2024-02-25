3f9c2300fe802d87351dd8c774402b1e3249db00
jitheshm <jitheshmjithooz@gmail.com>
2024-02-25 21:23:50 +0530

    admin apis for coupon management are added

--- backend/src/adapters/controllers/couponController.js
+++ backend/src/adapters/controllers/couponController.js
@@ -1,8 +1,20 @@
 const addCoupon = require("../../usecase/coupon/addCoupon")
+const getAllCoupon = require("../../usecase/coupon/getAllCoupon")
+const getCoupon = require("../../usecase/coupon/getCoupon")
+const updateCoupon = require("../../usecase/coupon/updateCoupon")
 const couponRepository = require("../repositories/couponRepository")
 
 module.exports={
     addCoupon:async(data)=>{
         return await addCoupon(data,couponRepository)
+    },
+    getCoupon:async(id)=>{
+        return await getCoupon(id,couponRepository)
+    },
+    updateCoupon:async(data)=>{
+        return await updateCoupon(data,couponRepository)
+    },
+    getAllCoupon:async()=>{
+        return await getAllCoupon(couponRepository)
     }
 }
