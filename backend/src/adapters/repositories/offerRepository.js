1d72a9513d28de7fb0d91546bf1a8b73d316803d
jitheshm <jitheshmjithooz@gmail.com>
2024-02-27 13:03:39 +0530

    offers list in admin panel

--- backend/src/adapters/repositories/offerRepository.js
+++ backend/src/adapters/repositories/offerRepository.js
@@ -1,9 +1,16 @@
-const OfferModel=require('../models/offerModel');
+const OfferModel = require('../models/offerModel');
 module.exports = {
     addOffer: (data) => {
         try {
             const offer = new OfferModel(data);
             offer.save();
+        } catch (err) {
+            throw new Error(err);
+        }
+    },
+    getAllOffers: async () => {
+        try {
+            return await OfferModel.find();
         } catch (err) {
             throw new Error(err);
         }
---
+++ frontend/admin/src/components/OffersList/OffersList.jsx
@@ -0,0 +1,118 @@
+import React, { useEffect, useState } from 'react'
+import instance from '../../axios'
+import Cookies from 'js-cookie';
+import { Link } from 'react-router-dom';
+
+
+function OffersList() {
+
+    const [offers, setOffers] = useState([])
+    const [search, setSearch] = useState("")
+
+    useEffect(() => {
+        instance.get('/admin/getalloffers', {
+            headers: {
+                Authorization: Cookies.get('token')
+            }
+        }).then((res) => {
+            console.log(res);
+
+            setOffers(res.data.data)
+        })
+    }, [])
+
+    const handleSearch = (e) => {
+        setSearch(e.target.value)
+    }
+
+    const handleDelete = (id) => {
+        if (confirm('Are you sure you want to delete this coupon?')) {
+            instance.delete(`/admin/deletecoupon/${id}`, {
+                headers: {
+                    Authorization: Cookies.get('token')
+                }
+            }).then((res) => {
+                console.log(res);
+                setOffers(offers.filter((coupon) => {
+                    return coupon.couponId !== id
+                }))
+            })
+        }
+    }
+    return (
+        <>
+            <div className='pt-5'>
+                <div className="col-lg-11 mt-5 m-auto grid-margin stretch-card">
+                    <div className="card">
+                        <div className="card-body">
+                            <div className='row align-items-center mb-4'>
+                                <h4 className="card-title col-4 mt-4">offers</h4>
+                                <form className="nav-link  d-none d-lg-flex search col-6">
+                                    <input type="text" className="form-control" placeholder="Search offers" style={{ color: "white" }} onChange={handleSearch} />
+                                </form>
+                            </div>
+
+
+                            <div className="table-responsive col-12">
+                                <table className="table text-center" >
+                                    <thead>
+                                        <tr>
+                                            <th style={{ width: "50px" }}>Index</th>
+                                            <th>Offer Title</th>
+                                            <th>startDate</th>
+                                            <th>endDate</th>
+                                            <th>Discount Type</th>
+                                            <th>Discount</th>
+                                            <th>offerType</th>
+                                            <th>applicables</th>
+                                            <th style={{ textAlign: "center", width: "200px" }}>Actions</th>
+
+                                        </tr>
+                                    </thead>
+                                    <tbody >
+                                        {
+                                            offers.map((offer, index) => {
+                                                if (offer.offerTitle.startsWith(search) || search === "") {
+                                                    return (
+                                                        // eslint-disable-next-line react/jsx-key
+                                                        <tr >
+                                                            <td style={{ color: "#bcc0d7", width: "50px" }}>{index + 1}</td>
+                                                            <td style={{ color: "#bcc0d7" }}>{offer.offerTitle}</td>
+                                                            <td style={{ color: "#bcc0d7" }}>{new Date(offer.startDate).toDateString()} {new Date(offer.startDate).toLocaleTimeString()}</td>
+                                                            <td style={{ color: "#bcc0d7" }}>{new Date(offer.endDate).toDateString()} {new Date(offer.endDate).toLocaleTimeString()}</td>
+
+                                                            <td style={{ color: "#bcc0d7" }}>{offer.discountType}</td>
+                                                            <td style={{ color: "#bcc0d7" }}>{offer.discount}</td>
+                                                            <td style={{ color: "#bcc0d7" }}>{offer.offerType}</td>
+                                                            <td style={{ color: "#bcc0d7" }}>{offer.applicables}</td>
+                                                            <td className='d-flex gap-3 justify-content-center' style={{ width: "200px" }}>
+
+                                                                <Link to={`/editcoupon/${offer._id}`} className='btn btn-outline-warning'>Edit</Link>
+
+
+                                                                <button className='btn btn-outline-danger' onClick={() => handleDelete(offer._id)}>Delete</button>
+
+
+                                                            </td>
+                                                        </tr>
+                                                    )
+                                                } else {
+                                                    return null
+                                                }
+                                            })
+                                        }
+
+                                    </tbody>
+                                </table>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+        </>
+    )
+
+}
+
+export default OffersList
