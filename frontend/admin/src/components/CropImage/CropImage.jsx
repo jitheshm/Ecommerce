f7d092a08eb6c9028675e74f171d14cf0fe2b2e3
jitheshm <jitheshmjithooz@gmail.com>
2024-03-11 09:30:50 +0530

    image cropping feature updated

--- frontend/admin/src/components/CropImage/CropImage.jsx
+++ frontend/admin/src/components/CropImage/CropImage.jsx
@@ -1,126 +1,21 @@
 
 
-import React, { useState } from 'react'
-import Cropper from 'react-easy-crop'
+import React, { useRef, useState } from 'react'
+// import Cropper from 'react-easy-crop'
 import './CropImage.css'
 import { BASEURL } from "../../constants/constant.json"
-import Resizer from "react-image-file-resizer";
+// import Resizer from "react-image-file-resizer";
+import Cropper from "react-cropper";
+import "cropperjs/dist/cropper.css";
 function CropImage({ src, setImage, setImgPrev, setCropWindow, id, setOldImage }) {
 
 
-    const [crop, setCrop] = useState({ x: 0, y: 0 })
-    const [rotation, setRotation] = useState(0)
-    const [zoom, setZoom] = useState(1)
-    const [croppedAreaPixels, setCroppedAreaPixels] = useState(null)
-    //const [croppedImage, setCroppedImage] = useState(null)
-
-
-
-    const createImage = (url) =>
-        new Promise((resolve, reject) => {
-            const image = new Image()
-            image.addEventListener('load', () => resolve(image))
-            image.addEventListener('error', (error) => reject(error))
-            image.setAttribute('crossOrigin', 'anonymous') // needed to avoid cross-origin issues on CodeSandbox
-            image.src = url
-        })
-    const getRadianAngle = (degreeValue) => {
-        return (degreeValue * Math.PI) / 180
-    }
-
-    /**
-     * Returns the new bounding area of a rotated rectangle.
-     */
-    const rotateSize = (width, height, rotation) => {
-        const rotRad = getRadianAngle(rotation)
-
-        return {
-            width:
-                rotation === 0 ? 353 : Math.abs(Math.cos(rotRad) * width) + Math.abs(Math.sin(rotRad) * height),
-            height:
-                rotation === 0 ? 416 : Math.abs(Math.sin(rotRad) * width) + Math.abs(Math.cos(rotRad) * height),
-        }
-    }
-
-    /**
-     * This function was adapted from the one in the ReadMe of https://github.com/DominicTobias/react-image-crop
-     */
-    /**
-     * This function was adapted from the one in the ReadMe of https://github.com/DominicTobias/react-image-crop
-     */
-    const getCroppedImg = async (
-        imageSrc,
-        pixelCrop,
-        rotation = 0,
-        flip = { horizontal: false, vertical: false }
-    ) => {
-        const image = await createImage(imageSrc)
-        const canvas = document.createElement('canvas')
-        const ctx = canvas.getContext('2d')
-
-        if (!ctx) {
-            return null
-        }
-
-        const rotRad = getRadianAngle(rotation)
-
-        // calculate bounding box of the rotated image
-        const { width: bBoxWidth, height: bBoxHeight } = rotateSize(
-            image.width,
-            image.height,
-            rotation
-        )
-
-        // set canvas size to match the bounding box
-        canvas.width = bBoxWidth
-        canvas.height = bBoxHeight
-        ctx.fillStyle = 'white';
-        ctx.fillRect(0, 0, canvas.width, canvas.height);
-        // translate canvas context to a central location to allow rotating and flipping around the center
-        ctx.translate(bBoxWidth / 2, bBoxHeight / 2)
-        ctx.rotate(rotRad)
-        ctx.scale(flip.horizontal ? -1 : 1, flip.vertical ? -1 : 1)
-        ctx.translate(-image.width / 2, -image.height / 2)
-
-        // draw rotated image
-        ctx.drawImage(image, 0, 0)
-
-        const croppedCanvas = document.createElement('canvas')
-
-        const croppedCtx = croppedCanvas.getContext('2d')
-
-        if (!croppedCtx) {
-            return null
-        }
-
-        // Set the size of the cropped canvas
-        croppedCanvas.width = pixelCrop.width
-        croppedCanvas.height = pixelCrop.height
-        console.log(canvas.width, canvas.height);
-        croppedCtx.fillStyle = 'white';
-        croppedCtx.fillRect(0, 0, croppedCanvas.width, croppedCanvas.height);
-        // Draw the cropped image onto the new canvas
-        croppedCtx.drawImage(
-            canvas,
-            pixelCrop.x,
-            pixelCrop.y,
-            pixelCrop.width,
-            pixelCrop.height,
-            0,
-            0,
-            pixelCrop.width,
-            pixelCrop.height
-        )
-
-        // As Base64 string
-        // return croppedCanvas.toDataURL('image/jpeg');
-
-        // As a blob
-
-         croppedCanvas.toBlob(async (file) => {
-
-            const url = URL.createObjectURL(file)
-            setImage(file)
+    const cropperRef = useRef()
+    const onCrop = () => {
+        const cropper = cropperRef.current?.cropper;
+        cropper.getCroppedCanvas().toBlob((blob) => {
+            setImage(blob);
+            //setImgPrev(URL.createObjectURL(blob));
             setImgPrev((prev) => {
                 if (id && prev) {
                     console.log("iam here");
@@ -131,77 +26,80 @@ function CropImage({ src, setImage, setImgPrev, setCropWindow, id, setOldImage }
                     })
 
                 }
-                return url
+                return URL.createObjectURL(blob)
             })
-            setCropWindow(false)
-            // setOldImage(url)
-        })
-
+            setCropWindow(false);
+        });
+    };
+    const handleLeftRotate = () => {
+        const imageElement = cropperRef?.current;
+        const cropper = imageElement?.cropper;
+        cropper.rotate(-20);
+    }
+    const handleRightRotate = () => {
+        const imageElement = cropperRef?.current;
+        const cropper = imageElement?.cropper;
+        cropper.rotate(20);
     }
 
-
-
-
-
-    const onCropComplete = (croppedArea, croppedAreaPixels) => {
-        setCroppedAreaPixels(croppedAreaPixels)
+    const handleMove = () => {
+        const imageElement = cropperRef?.current;
+        const cropper = imageElement?.cropper;
+        cropper.setDragMode('move')
+    }
+    const handleCrop = () => {
+        const imageElement = cropperRef?.current;
+        const cropper = imageElement?.cropper;
+        cropper.setDragMode('crop')
     }
 
-    const onDone = async () => {
-        try {
-            await getCroppedImg(
-                src,
-                croppedAreaPixels,
-                rotation
-            )
-
-        } catch (e) {
-            console.error(e)
-        }
+    const handleReset = () => {
+        const imageElement = cropperRef?.current;
+        const cropper = imageElement?.cropper;
+        cropper.reset()
     }
-
-
-
 
 
     return (
         <>
-            <div className='cropParent col-5 '>
-
-
-                <div className='cropContainer'>
-                    <Cropper
-                        image={src}
-                        crop={crop}
-                        zoom={zoom}
-                        rotation={rotation}
-                        aspect={353 / 416}
-                        onCropChange={setCrop}
-                        onCropComplete={onCropComplete}
-                        onZoomChange={setZoom}
-                        onRotationChange={setRotation}
-                        objectFit='contain'
-                        restrictPosition={false}
-                        cropSize={{ width: 353, height: 416 }}
-                        style={{ containerStyle: { background: 'white' } }}
-
-                    />
-                </div>
-                <div className='controls'>
-                    <div >
-                        <div style={{ display: 'flex', gap: 5 }}>
-                            <label htmlFor="customRange1" className="form-label">Rotation</label>
-                            <input type="range" className="form-range" id="customRange1" min={0} max={360} step={1} onChange={(e) => {
-                                console.log(rotation);
-                                setRotation(e.target.value)
-                            }} value={rotation} />
+            <div className='cropParent col-10 py-4 '>
+                <div className='col-2'>
+                    <div className='col-3 m-auto py-4' style={{ height: "max-content", border: "solid", borderColor: "white", borderRadius: "40px", borderWidth: "1px" }}>
+                        <div className='m-auto d-flex' style={{ width: "min-content" }} onClick={handleMove}>
+                            <i className="fa-solid fa-up-down-left-right" style={{ fontSize: "25px" }} title='Move'/>
+                        </div>
+                        <div className='m-auto d-flex mt-4' style={{ width: "min-content" }} >
+                            <i className="fa-solid fa-crop" style={{ fontSize: "25px" }} onClick={handleCrop} title='Crop'/>
+                        </div>
+                        <div className='m-auto d-flex mt-4' style={{ width: "min-content" }}>
+                            <i className="fa-solid fa-rotate-left" style={{ fontSize: "25px" }} onClick={handleLeftRotate} title='Rotate left'/>
+                        </div>
+                        <div className='m-auto d-flex mt-4' style={{ width: "min-content" }} >
+                            <i className="fa-solid fa-rotate-right" style={{ fontSize: "25px" }} onClick={handleRightRotate} title='Rotate right'/>
+                        </div>
+                        <div className='m-auto d-flex mt-4' style={{ width: "min-content" }} >
+                            <i className="fa-solid fa-rotate" style={{ fontSize: "25px" }} onClick={handleReset} title='Reset'/>
+                        </div>
+                        <div className='m-auto d-flex mt-4' style={{ width: "min-content" }} >
+                            <i className="fa-solid fa-check" style={{ fontSize: "25px" }} onClick={onCrop} title='Done'/>
                         </div>
 
                     </div>
-                    <button type='button' className='btn btn-primary' onClick={() => {
-                        onDone()
-                    }}>Done</button>
+
                 </div>
+
+                <div className='cropContainer col-10 d-flex align-items-center'>
+                    <Cropper
+                        src={src}
+                        style={{ height: "100%", width: "100%" }}
+                        // Cropper.js options
+                        aspectRatio={353 / 416}
+                        guides={false}
+
+                        ref={cropperRef}
+                    />
+                </div>
+
             </div>
 
 
