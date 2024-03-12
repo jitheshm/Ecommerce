204c7a42708d11198868ab439511e61301f22822
jitheshm <jitheshmjithooz@gmail.com>
2024-03-12 20:04:13 +0530

    search result page design updated

--- frontend/user/src/components/SearchCard/SearchCard.jsx
+++ frontend/user/src/components/SearchCard/SearchCard.jsx
@@ -3,45 +3,46 @@ import { BASEURL } from "../../constants/constant.json"
 import { Link } from 'react-router-dom'
 function SearchCard({product}) {
     return (
-        <Link to={`/product/${product.productId}/${product.color}`} className='card col-12 mb-3 mt-3'>
+        <Link to={`/product/${product.productId}/${product.color}`} className="card mt-5 mt-md-0 mb-3">
 
 
-            <div className=" d-flex flex-row  ">
-                <div className='p-5 col-3'>
-                    <img className="card-img-top " src={product ? BASEURL + "/" + product.imagesUrl[0] : noImage} alt="Card image cap" style={{ height: "120px", width: "100px" }} />
-                </div>
-                <div className="card-body pt-4 col-6 ms-2 mt-2">
-                    <h4 className="card-title "><b>{product.productDetails[0].productName}</b></h4>
-                    <div className="d-flex flex-row">
-                        <div className="text-warning mb-1 me-2">
-                            <i className="fa fa-star" />
-                            <i className="fa fa-star" />
-                            <i className="fa fa-star" />
-                            <i className="fa fa-star" />
-                            <i className="fas fa-star-half-alt" />
-                            <span className="ms-1">
-                                4.5
-                            </span>
-                        </div>
+           
+                <div className='d-flex flex-row'>
 
+
+                    <div className='col-5 col-sm-2 py-5 ps-2'>
+                        <img className="card-img-top " src={product ? BASEURL + "/" + product.imagesUrl[0] : ""} alt="Card image cap" />
                     </div>
+                    <div className="card-body pt-4 col-6 col-md-6 ms-2 mt-2">
+                        <h4 className="card-title "><>{product.productDetails[0].productName}</></h4>
+                        <div className="d-flex flex-row">
+                            <div className="text-warning mb-1 me-2">
+                                <i className="fa fa-star" />
+                                <i className="fa fa-star" />
+                                <i className="fa fa-star" />
+                                <i className="fa fa-star" />
+                                <i className="fas fa-star-half-alt" />
+                                <span className="ms-1">
+                                    4.5
+                                </span>
+                            </div>
 
-                    <div className='mt-3'>    
+                        </div>
+                        <div className='mt-3'>    
                         <ul>
                             <li>{product.color} color</li>
                             <li>{product.productDetails[0].waranty} year Waranty</li>
                         </ul>
 
                     </div>
-                    <div style={{ height: "30px" }}>{product.stock===0 && <p style={{ color: "red" }}>Out of stock</p>}</div>
-                </div>
 
-                <div className='col-2'>   
-                    <h3><b>₹ {product.salePrice-product.discount}</b></h3>    
-                    <h5><s>₹ {product.salePrice}</s><b className='col-3 ms-3' style={{ color: "green" }}>20% OFF</b> </h5>
+                        <p className="card-text mt-2 row"><h4 className='col-md-5'><b>₹ {product.salePrice-product.discount}</b></h4></p>
+                        
+                    </div>
+                    
                 </div>
-
-            </div>
+                
+           
 
 
 
