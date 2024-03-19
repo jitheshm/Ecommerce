0786044ec6f66affe83ebfd70e9b86f4eb25a8f2
jitheshm <jitheshmjithooz@gmail.com>
2024-03-19 14:38:44 +0530

    docker is added for development and production environments, nginx added for reverse proxy in production

--- frontend/user/vite.config.js
+++ frontend/user/vite.config.js
@@ -5,4 +5,12 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   base: '/',
   plugins: [react()],
+  server: {
+    host: true,
+    port: 8000, // This is the port which we will use in docker
+    
+     watch: {
+       usePolling: true
+     }
+  }
 })
