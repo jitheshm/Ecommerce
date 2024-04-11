1d8e34b3c97f08c44487053a9e9c4d4c7349654e
jitheshm <jitheshmjithooz@gmail.com>
2024-04-11 15:39:58 +0530

    docker files and server file updated for production

--- backend/src/frameworks/webserver/server.js
+++ backend/src/frameworks/webserver/server.js
@@ -12,6 +12,9 @@ const path = require('path');
 
 const app = express();
 
+const userBuildPath = path.join(__dirname, '../../../user/dist');
+const adminBuildPath = path.join(__dirname, '../../../admin/dist')
+
 app.use(logger('dev'));
 app.use(express.json());
 const cors = require('cors');
@@ -19,6 +22,8 @@ const socketHandler = require('./socketHandler/socketHandler');
 app.use(cors());
 app.use(fileUpload());
 app.use('/public', express.static('public'));
+app.use('/', express.static(userBuildPath));
+app.use('/adminpanel', express.static(adminBuildPath));
 
 db.connect();
 app.use(passport.initialize());
@@ -26,18 +31,42 @@ app.use(passport.initialize());
 app.use('/user', userRouter);
 app.use('/admin', adminRouter);
 
-const server = http.createServer(app); // Create HTTP server
-const io = socketIo(server,{
-    cors: {
-      origin: '*',
+app.get("/adminpanel/*", function (req, res) {
+  console.log("admin");
+  res.sendFile(
+    path.join(__dirname, "../../../admin/dist/index.html"),
+    function (err) {
+      if (err) {
+        res.status(500).send(err);
+      }
     }
-  }); // Attach Socket.IO to the HTTP server
+  );
+});
+
+app.get("/*", function (req, res) {
+  console.log("user");
+  res.sendFile(
+    path.join(__dirname, "../../../user/dist/index.html"),
+    function (err) {
+      if (err) {
+        res.status(500).send(err);
+      }
+    }
+  );
+});
+
+const server = http.createServer(app); // Create HTTP server
+const io = socketIo(server, {
+  cors: {
+    origin: '*',
+  }
+}); // Attach Socket.IO to the HTTP server
 
 // Socket.IO logic
-io.on('connection', socket=>{
-  socketHandler(socket,io)
+io.on('connection', socket => {
+  socketHandler(socket, io)
 });
 
 server.listen(port, () => {
-    console.log(`Server running in port ${port}`);
+  console.log(`Server running in port ${port}`);
 });
