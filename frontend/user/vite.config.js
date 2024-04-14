5886aa3221f2654a6472f3f2c375fb80116fd791
jitheshm <jitheshmjithooz@gmail.com>
2024-04-14 20:32:16 +0530

    Merge branch 'main' into render

--- frontend/user/vite.config.js
+++ frontend/user/vite.config.js
@@ -1,16 +1,41 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { VitePWA } from 'vite-plugin-pwa'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   base: '/',
-  plugins: [react()],
+  plugins: [react(), VitePWA({
+    registerType: 'autoUpdate',
+    devOptions: {
+      enabled: true
+    },
+    manifest: {
+        name: 'Electro',
+        short_name: 'Electro',
+        description: 'My Awesome App description',
+        theme_color: '#15161D',
+        start_url: '/',
+        icons: [
+          {
+            src: 'icon-192x192.png',
+            sizes: '192x192',
+            type: 'image/png'
+          },
+          {
+            src: 'icon-512x512.png',
+            sizes: '512x512',
+            type: 'image/png'
+          }
+        ]
+      }
+  })],
   server: {
     host: true,
     port: 8000, // This is the port which we will use in docker
-    
-     watch: {
-       usePolling: true
-     }
+
+    watch: {
+      usePolling: true
+    }
   }
 })
