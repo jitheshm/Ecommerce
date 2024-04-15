093ac66048885e92803615703a2cc70d5a1140c4
jitheshm <jitheshmjithooz@gmail.com>
2024-04-15 10:38:32 +0530

    production update

--- frontend/user/vite.config.js
+++ frontend/user/vite.config.js
@@ -6,29 +6,35 @@ import { VitePWA } from 'vite-plugin-pwa'
 export default defineConfig({
   base: '/',
   plugins: [react(), VitePWA({
+    workbox: {
+      navigateFallbackDenylist: [
+        /^\/user\/auth\/google/,
+        /^\/adminpanel/
+      ]
+    },
     registerType: 'autoUpdate',
     devOptions: {
       enabled: true
     },
     manifest: {
-        name: 'Electro',
-        short_name: 'Electro',
-        description: 'My Awesome App description',
-        theme_color: '#15161D',
-        start_url: '/',
-        icons: [
-          {
-            src: 'icon-192x192.png',
-            sizes: '192x192',
-            type: 'image/png'
-          },
-          {
-            src: 'icon-512x512.png',
-            sizes: '512x512',
-            type: 'image/png'
-          }
-        ]
-      }
+      name: 'Electro',
+      short_name: 'Electro',
+      description: 'My Awesome App description',
+      theme_color: '#15161D',
+      start_url: '/',
+      icons: [
+        {
+          src: 'icon-192x192.png',
+          sizes: '192x192',
+          type: 'image/png'
+        },
+        {
+          src: 'icon-512x512.png',
+          sizes: '512x512',
+          type: 'image/png'
+        }
+      ]
+    }
   })],
   server: {
     host: true,
